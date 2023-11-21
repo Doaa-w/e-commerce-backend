9e3207edb481f721f68f93e9fe4cc2f6bf2e82c2
.ドウアア. <dia13@outlook.sa>
2023-11-22 00:14:06 +0300

    adjustments

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,8 @@ import { dev } from ".";
 export const connectDB= async ()=>{
 try {
     await mongoose.connect(dev.db.url);
+    console.log('Database is connected')
 } catch (error) {
-   console.log(error) ;
+   console.log('MongoDB connection error ', error) ;
 }
 };
--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import { connectDB } from './config/db'
 
 const app: Application = express()
 const port:number = dev.app.port
-const URL = dev.db.url  as string
+// const URL = dev.db.url  as string
 
 app.use(myLogger)
 app.use(express.urlencoded({ extended: false }))
@@ -25,14 +25,14 @@ app.use('/api/products', productsRouter)
 
 app.use(apiErrorHandler)
 
-mongoose
-  .connect(URL)
-  .then(() => {
-    console.log('Database connected')
-  })
-  .catch((err) => {
-    console.log('MongoDB connection error, ', err)
-  })
+// mongoose
+//   .connect(URL)
+//   .then(() => {
+//     console.log('Database is connected')
+//   })
+//   .catch((err) => {
+//     console.log('MongoDB connection error, ', err)
+//   })
 
 app.listen(port, async () => {
   console.log('Server running http://localhost:' + port)
