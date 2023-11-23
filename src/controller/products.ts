6876de58897fa10467eb6450142458e08600563a
Amal Khardli <amalalikhardli@gmail.com>
2023-11-23 11:59:04 +0300

    change based on comments

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,71 +1,29 @@
-import {Request,Response , NextFunction } from "express";
-import product from "../models/product";
-import { productInput } from "../types";
+import { Request, Response, NextFunction } from 'express'
+import product from '../models/product'
 
-
-export const getAllProducts = async (req: Request , res: Response , next:NextFunction)=>{
-    try {
-      const products = await product.find()
-        res.status(200).json({message: 'all products are here' , payload:products})  
-    } catch (error) {
-        next(error)
-    }
-
+export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const products = await product.find()
+    res.status(200).json({ message: 'All products are here', payload: products })
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const createProduct = async (req: Request , res: Response , next:NextFunction)=>{
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, description, quantity } = req.body
-    const s_product = new product({
+    const c_product = new product({
       name,
       description,
       quantity,
     })
-    await s_product.save()
-    console.log(s_product)
+    await c_product.save()
+    console.log(c_product)
 
-    res.status(201).json({ message: 'all products are here', payload: s_product })
-  } catch (error) {
-    next(error)
-  }
-}
-export const deleteProduct = async (req: Request , res: Response , next:NextFunction)=> {
-  try {
-    const id = req.params.id
-    const deletedProduct = await product.findByIdAndDelete(id)
-    if (deletedProduct) {
-      res.status(200).json({
-        message: 'Product deleted',
-        payload: deletedProduct,
-      })
-    }
+    res.status(201).json({ message: 'Product created successfully', payload: c_product })
   } catch (error) {
     next(error)
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-    // const { name, description, quantity } = req.body
-
-    // if (!name || !description) {
-    //   next(ApiError.badRequest('Name and Description are requried'))
-    //   return
-    // }
-    // const product = new Product({
-    //   name,
-    //   description,
-    //   quantity,
-    // })
-  
-    // await product.save()
-    // res.json(product)
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -4,10 +4,12 @@ import Product from '../models/product'
 import Order from '../models/order'
 import ApiError from '../errors/ApiError'
 
-import { createProduct, deleteProduct, getAllProducts } from '../controller/products';
+import { createProduct, deleteProduct,  getAllProducts, getSingleProduct } from '../controller/products';
 
 const router = Router()
 router.get('/', getAllProducts)
+
+router.get('/:id', getSingleProduct)
 
 router.post('/', createProduct)
 
