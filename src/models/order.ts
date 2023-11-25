dbd226e77962f4884c41a05d6194da3413e8fc37
AtheerAlfaya <hp-atheer20@outlook.sa>
2023-11-25 21:00:49 +0300

    order CRUD

--- src/models/order.ts
+++ src/models/order.ts
@@ -5,6 +5,7 @@ export type OrderDocument = Document & {
   name: string;
   products: mongoose.Schema.Types.ObjectId[];
   slug: string;
+  user: string; // Reference to User model using slug
 };
 
 const orderSchema = new mongoose.Schema({
@@ -12,16 +13,24 @@ const orderSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  products: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: 'Product',
-  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+  ],
   slug: {
     type: String,
     unique: true,
   },
-}) ;
+  user: {
+    type: String, // Use the slug of the User model
+    ref: 'User',
+    required: true,
+  },
+});
 
+// Middleware to automatically generate and set the slug before saving the order
 orderSchema.pre<OrderDocument>('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
