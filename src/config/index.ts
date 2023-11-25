baac58abc2757fe6758d9aac62222fa7eaab9729
Abeer Alnakhli <iiberu0@hotmail.com>
2023-11-26 01:03:58 +0300

    code formatting

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,10 @@
-import "dotenv/config"
+import "dotenv/config";
 
 export const dev ={
-    app:{port: Number(process.env.PORT) || 3003},
-    db:{url : process.env.MONGODB_URL ||  'mongodb://localhost:27017/ecommerce-db'}
-}
+    app: {
+        port: Number(process.env.PORT) || 3003
+    },
+    db: {
+        url : process.env.MONGODB_URL ||  'mongodb://localhost:27017/ecommerce-db'
+    }
+};
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import express, { Request, Response, NextFunction, Application } from "express";
 import { dev } from "./config";
 import { connectDB } from './config/db';
 
-import usersRouter from './routers/users'
-import productsRouter from './routers/products'
-import ordersRouter from './routers/orders'
-import categoryRouter from './routers/categories'
+import usersRouter from './routers/users';
+import productsRouter from './routers/products';
+import ordersRouter from './routers/orders';
+import categoryRouter from './routers/categories';
 
-import apiErrorHandler from './middlewares/errorHandler'
-import myLogger from './middlewares/logger'
+import apiErrorHandler from './middlewares/errorHandler';
+import myLogger from './middlewares/logger';
 
 const app: Application = express();
 const port:number = dev.app.port;
@@ -31,7 +31,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
-app.use('/api/categories', categoryRouter)
+app.use('/api/categories', categoryRouter);
 
 app.use(apiErrorHandler);
 
@@ -45,6 +45,6 @@ app.use(apiErrorHandler);
 //   })
 
 app.listen(port, async () => {
-  console.log('Server running http://localhost:' + port)
-   connectDB();
+  console.log('Server running http://localhost:' + port);
+  connectDB();
 });
