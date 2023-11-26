b68f78f1cc4daf2887000262f61b4b50c012f082
Abeer Alnakhli <iiberu0@hotmail.com>
2023-11-26 03:03:05 +0300

    create user

---
+++ src/services/userService.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request } from "express";
+
+import ApiError from "../errors/ApiError";
+import User from "../models/user";
+
+export const userExist = async (email: string) => {
+    const userExist = await User.exists({email: email});
+    if (userExist) {
+        const error = new ApiError(409, "User account already exists")
+        throw error;       
+    }
+}
+
+export const createUser = async (req: Request, next: NextFunction) => {
+    const { first_name, last_name, email, password, phone, address } = req.body;
+    await userExist(email);
+    const user = new User ({
+        first_name,
+        last_name,
+        email,
+        password,
+        phone,
+        address
+    });
+    await user.save();
+    return user;
+}
