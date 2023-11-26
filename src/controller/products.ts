49a3a767445f57898ac2f563f3d57d5189a00b1f
Amal Khardli <amalalikhardli@gmail.com>
2023-11-26 06:36:47 +0300

    update the model + update the CRUD products + make the pagination

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,29 +1,116 @@
 import { Request, Response, NextFunction } from 'express'
-import product from '../models/product'
+import Product from '../models/product'
+import slugify from 'slugify'
 
 export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const products = await product.find()
-    res.status(200).json({ message: 'All products are here', payload: products })
-  } catch (error) {
-    next(error)
-  }
-}
+    let page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const totalCount = await Product.countDocuments()
+    const totalPages = Math.ceil(totalCount / limit)
+    if (page > totalPages) {
+      page = totalPages > 0 ? totalPages : 1
+    }
 
-export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { name, description, quantity } = req.body
-    const c_product = new product({
-      name,
-      description,
-      quantity,
+    const skip = (page - 1) * limit
+    const products = await Product.find().skip(skip).limit(limit)
+
+    if (page > totalCount) {
+    }
+
+    res.status(200).json({
+      message: 'Returns all products',
+      payload: products,
+      page,
+      limit,
+      totalCount,
     })
-    await c_product.save()
-    console.log(c_product)
-
-    res.status(201).json({ message: 'Product created successfully', payload: c_product })
   } catch (error) {
     next(error)
   }
 }
 
+export const getSingleProductsBySlug = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const slug = req.params.slug
+    const product = await Product.findOne({ slug: slug })
+    // send a status code to Error
+    if (!product) {
+      throw new Error('Product not found!')
+    }
+    console.log(product)
+
+    res.status(200).json({
+      message: 'Product found',
+      payload: product,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const deleteSingleProducts = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const slug = req.params.slug
+    const deletedProduct = await Product.findOneAndDelete({ slug: slug })
+
+    if (!deletedProduct) {
+      throw new Error('Product not found!')
+    }
+
+    res.json({
+      message: 'Product deleted',
+      payload: deletedProduct,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const createSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const newProduct = req.body
+    const productExist = await Product.exists({ title: newProduct.title })
+    if (productExist) {
+      return res.status(400).json({ message: 'Product already exists' })
+    }
+    newProduct.slug = slugify(newProduct.title, { lower: true })
+    const product = new Product(newProduct)
+    await product.save()
+    res.json({
+      message: 'Product created',
+      payload: product,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+export const updateSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const originalSlug = req.params.slug
+    const { title } = req.body
+    const updateProductData = req.body
+
+
+    const productExists = await Product.findOne({ slug: originalSlug })
+    if (!productExists) {
+      return res.status(404).json({ message: 'Product not found!' })
+    }
+
+
+    if (title && title !== productExists.title) {
+      updateProductData.slug = slugify(title, { lower: true })
+    }
+
+    const updatedProduct = await Product.findOneAndUpdate(
+      { slug: originalSlug },
+      updateProductData,
+      { new: true } 
+    )
+
+    res.json({
+      message: 'Product updated successfully',
+      payload: updatedProduct,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
+import morgan from 'morgan'
 
 import usersRouter from './routers/users'
 import productsRouter from './routers/products'
@@ -9,20 +10,23 @@ import apiErrorHandler from './middlewares/errorHandler'
 import myLogger from './middlewares/logger'
 import {dev} from "./config"
 import { connectDB } from './config/db'
+import createError from 'http-errors'
 
 // import { config } from 'dotenv'
 config()
 
 const app: Application = express()
 const port:number = dev.app.port
-// const URL = dev.db.url  as string
+const URL = dev.db.url  as string
+
+app.use(morgan('dev'))
 
 app.use(myLogger)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use('/api/users', usersRouter)
-app.use('/api/orders', ordersRouter)
+// app.use('/api/users', usersRouter)
+// app.use('/api/orders', ordersRouter)
 app.use('/api/products', productsRouter)
 
 app.use(apiErrorHandler)
@@ -40,3 +44,10 @@ app.listen(port, async () => {
   console.log('Server running http://localhost:' + port)
    connectDB();
 })
+
+// throw Error
+// app.use((req, res, next) => {
+//   next(createError(404, 'Not Found'))
+// })
+//  errorHandler muat be the last one
+// app.use(apiErrorHandler)
