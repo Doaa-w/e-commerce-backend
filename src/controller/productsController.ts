0ca51512c1acd60c739594c6d9b9317781fba4be
Amal Khardli <amalalikhardli@gmail.com>
2023-12-03 09:10:36 +0300

    delete product:

--- src/controller/productsController.ts
+++ src/controller/productsController.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from 'express';
-import slugify from 'slugify';
+import { Request, Response, NextFunction } from 'express'
+import slugify from 'slugify'
 
 import {
   createProduct,
@@ -7,31 +7,31 @@ import {
   getProducts,
   getSingleProduct,
   updateProduct,
-} from '../services/productService';
-import Product from '../models/productSchema';
-import { dev } from '../config';
-import ApiError from '../errors/ApiError';
-import { createHttpError } from '../util/createHttpError';
+} from '../services/productService'
+import Product from '../models/productSchema'
+import { dev } from '../config'
+import ApiError from '../errors/ApiError'
+import { createHttpError } from '../util/createHttpError'
 
 export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    let page = Number(req.query.page);
-    const limit = Number(req.query.limit);
-    const search = req.query.search as string;
-    const result = await getProducts(page, limit, search);
+    let page = Number(req.query.page)
+    const limit = Number(req.query.limit)
+    const search = req.query.search as string
+    const result = await getProducts(page, limit, search)
     res.status(200).json({
       message: 'Returns all products',
       // payload: result.products,
       ...result,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export const getSingleProductById = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const id = req.params.id 
+    const id = req.params.id
     const product = await getSingleProduct(id)
     res.status(200).json({
       message: 'Product found',
@@ -44,18 +44,19 @@ export const getSingleProductById = async (req: Request, res: Response, next: Ne
 
 export const deleteSingleProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const slug = req.params.slug;
-    await deleteProduct(slug);
-    res.status(204).json();
+    const slug = req.params.slug
+    await deleteProduct(slug)
+    res.status(204).json()
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
+
 
 export const createSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { title, price, description, quantity, category, sold, shipping } = req.body;
-    const imagePath = req.file?.path;
+    const { title, price, description, quantity, category, sold, shipping } = req.body
+    const imagePath = req.file?.path
     const productInput = {
       title,
       slug: slugify(title),
@@ -67,47 +68,52 @@ export const createSingleProduct = async (req: Request, res: Response, next: Nex
       sold,
       shipping,
     }
-    const product = await createProduct(productInput);
+    const product = await createProduct(productInput)
     res.status(201).json({
       message: 'Product created',
-      payload: product
-    });
+      payload: product,
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export const updateSingleProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const originalSlug = req.params.slug;
-    const updateProductData = req.body;
-    const updatedProduct = await updateProduct(originalSlug, updateProductData);
+    const originalSlug = req.params.slug
+    let updateProductData = req.body
+
+    if (req.file) {
+      updateProductData.image = `/images/products/${req.file.filename}`
+    }
+
+    const updatedProduct = await updateProduct(originalSlug, updateProductData)
     res.status(200).json({
       message: 'Product updated successfully',
-      payload: updatedProduct
-    });
+      payload: updatedProduct,
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-// filter part : 
+// filter part :
 export const getFilteredProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { category, priceRangeMin, priceRangeMax } = req.query;
-    const filter: any = {};
+    const { category, priceRangeMin, priceRangeMax } = req.query
+    const filter: any = {}
     if (category) {
-      filter.category = category;
+      filter.category = category
     }
     if (priceRangeMin && priceRangeMax) {
-      filter.price = { $gte: Number(priceRangeMin), $lte: Number(priceRangeMax) };
+      filter.price = { $gte: Number(priceRangeMin), $lte: Number(priceRangeMax) }
     }
-    const products = await Product.find(filter);
+    const products = await Product.find(filter)
     res.status(200).json({
       message: 'Returns filtered products',
       payload: products,
-    });
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
--- src/routers/productsRoute.ts
+++ src/routers/productsRoute.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
 import {
   createSingleProduct,
@@ -8,19 +8,27 @@ import {
   getSingleProductById,
   updateSingleProduct,
 } from '../controller/productsController'
-import { upload } from '../middlewares/uploadFile';
+import { upload } from '../middlewares/uploadFile'
 
-import { isAdmin, isLoggedIn } from '../middlewares/auth';
-import { createProductValidation } from '../validation/vaildations';
-import { runValidation } from '../validation/runValidation';
+import { isAdmin, isLoggedIn } from '../middlewares/auth'
+import { createProductValidation } from '../validation/vaildations'
+import { runValidation } from '../validation/runValidation'
 
-const router = Router();
+const router = Router()
 
-router.get('/', getAllProducts);
+router.get('/', getAllProducts)
 router.get('/:id', getSingleProductById)
-router.get('/filtered-products', getFilteredProducts);
-router.post('/', isLoggedIn, /*isAdmin,*/ /*createProductValidation, runValidation,*/ upload.single('image'), createSingleProduct);
-router.put('/:slug', isLoggedIn, /*isAdmin,*/ /*createProductValidation, runValidation,*/ updateSingleProduct);
-router.delete('/:slug', isLoggedIn, /*isAdmin,*/ deleteSingleProducts);
+router.get('/filtered-products', getFilteredProducts)
+router.post(
+  '/',
+  /* isLoggedIn, isAdmin,*/ /*createProductValidation, runValidation,*/ upload.single('image'),
+  createSingleProduct
+)
+router.put(
+  '/:slug',
+  /* isLoggedIn, isAdmin,*/ /*createProductValidation, runValidation,*/ upload.single('image'),
+  updateSingleProduct
+)
+router.delete('/:slug', /* isLoggedIn, /*isAdmin,*/ deleteSingleProducts)
 
-export default router;
+export default router
